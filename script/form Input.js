d76b859a9a9b21ec08dfad4a96f3f8b8d474bafe
Chris <hristijan_popovski@yahoo.com>
2025-05-31 09:05:38 +0200

    A few fixes and added a success message on from submit

--- script/form Input.js
+++ script/form Input.js
@@ -1,6 +1,13 @@
-document.getElementById("contact-form").addEventListener("submit", (e) => {
+document.getElementById("contact-form").addEventListener("submit", function (e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
+    const popup = document.getElementById('successPopup');
+    popup.innerHTML = `Ви благодариме пораката е испратена!`;
+    popup.style.display = 'block';
+    this.reset();
+    setTimeout(() => {
+        popup.style.display = 'none';
+      }, 3000);
     console.log(data);
 })
